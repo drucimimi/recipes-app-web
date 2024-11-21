841a861df431f48e0695628998ddf6c69266b8d2
drucimimi <drucilladeroche@protonmail.com>
2024-11-21 18:24:40 +0100

    Test pour check error access external url dans APK

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ const Home = () => {
           Téléchargez notre application mobile dès maintenant !
         </p>
         <div className="flex flex-col lg:flex-row gap-2">
-          <ButtonLink source='/download/apk/recipes-app-2024.1.4.apk' name="Télécharger l'application Android" action='Télécharger' icon={iconAPK} iconReverse={iconAPKReverse} iconDescription='Icone Android' /> 
+          <ButtonLink source='/download/apk/recipes-app-2024.1.5.apk' name="Télécharger l'application Android" action='Télécharger' icon={iconAPK} iconReverse={iconAPKReverse} iconDescription='Icone Android' /> 
         </div>
       </main>
       <Footer />
