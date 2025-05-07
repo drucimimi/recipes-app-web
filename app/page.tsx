5eece5abdf97fa708b53be28674ea23133b7f4c6
drucimimi <drucilladeroche@protonmail.com>
2025-05-07 18:42:45 +0200

    Nouvelle mini-version APK

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ const Home = () => {
           Téléchargez notre application mobile dès maintenant !
         </p>
         <div className="flex flex-col lg:flex-row gap-2">
-          <ButtonLink source='/download/apk/recipes-app-2025.1.1.apk' name="Télécharger l'application Android" action='Télécharger' icon={iconAPK} iconReverse={iconAPKReverse} iconDescription='Icone Android' />
+          <ButtonLink source='/download/apk/recipes-app-2025.1.2.apk' name="Télécharger l'application Android" action='Télécharger' icon={iconAPK} iconReverse={iconAPKReverse} iconDescription='Icone Android' />
         </div>
         {/* <div className="flex flex-col lg:flex-row gap-2">
           <ButtonLink source='/web' name="Voir la version web" action='Voir' icon={iconWeb} iconReverse={iconWebReverse} iconDescription='Icone Web' />
