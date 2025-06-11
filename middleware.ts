576af203d862d8d16cc76f3e46017d1a91dd8fd2
drucimimi <drucilladeroche@protonmail.com>
2025-06-11 20:48:20 +0200

    correction expiration de strict transport policy

--- middleware.ts
+++ middleware.ts
@@ -32,7 +32,7 @@ export async function middleware(request: NextRequest) {
 
   // Définition des en-têtes de sécurité
   response.headers.set('Content-Security-Policy', csp)
-  response.headers.set('Strict-Transport-Security', 'max-age=63072000; includeSubDomains; preload')
+  response.headers.set('Strict-Transport-Security', 'max-age=6307200000; includeSubDomains; preload')
   response.headers.set('X-Content-Type-Options', 'nosniff')
   response.headers.set('X-Frame-Options', 'DENY')
   response.headers.set('X-XSS-Protection', '1; mode=block')
